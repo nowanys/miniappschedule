f4abefd2ebce71afea225e92aeae6b0e31bc3156
xieyushi <75754567@qq.com>
2020-06-29 02:14:04 +0800

    1111

--- cloudfunctions-aliyun/common/config/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  appId: 'xxxx',
-  secret: 'xxxx',
-  //示例网址：https://static-678a6671-bf61-4dd4-b764-649d16978b86.bspapp.com/#/?id=
-//最后拼接出来的地址会是：https://static-678a6671-bf61-4dd4-b764-649d16978b86.bspapp.com/#/?id=5ef6a906d409e9004cdebec9
-  pcUrl:'xxxx'
-}
